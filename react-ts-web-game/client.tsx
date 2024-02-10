30977a660fd34119bab702f33e8c1dbb45834ac2
ymiru0324 <ymiru0324@naver.com>
2024-02-10 18:10:16 +0900

    fix. react-ts-web-game

---
+++ react-ts-web-game/client.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as  ReactDOM from 'react-dom';
+
+import GuGuDan from './1_GuGuDan/GuGuDan';
+import WordRelay from './2_WordRelay/WordRelay';
+
+ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
+// ReactDOM.render(<WordRelay />, document.querySelector('#root'));
