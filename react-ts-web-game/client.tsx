ce4b736614351734ee243a13d9b896552e81c8ed
ymiru0324 <ymiru0324@naver.com>
2024-02-12 17:55:50 +0900

    feat, NumberBaseball

--- react-ts-web-game/client.tsx
+++ react-ts-web-game/client.tsx
@@ -3,6 +3,8 @@ import * as  ReactDOM from 'react-dom';
 
 import GuGuDan from './1_GuGuDan/GuGuDan';
 import WordRelay from './2_WordRelay/WordRelay';
+import NumberBaseball from './3_NumberBaseball/NumberBaseball';
 
 // ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
-ReactDOM.render(<WordRelay />, document.querySelector('#root'));
+// ReactDOM.render(<WordRelay />, document.querySelector('#root'));
+ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
