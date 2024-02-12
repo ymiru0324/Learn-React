688fdb5c1101ea66db5b0c72d23614ea94835bd1
yongminiii <dev.yongmini@gmail.com>
2024-02-12 21:06:03 +0900

    feat. ResponseCheckClass

--- react-ts-web-game/client.tsx
+++ react-ts-web-game/client.tsx
@@ -4,7 +4,9 @@ import * as  ReactDOM from 'react-dom';
 import GuGuDan from './1_GuGuDan/GuGuDan';
 import WordRelay from './2_WordRelay/WordRelay';
 import NumberBaseball from './3_NumberBaseball/NumberBaseball';
+import ResponseCheck from './4_ResponseCheck/ResponseCheck';
 
 // ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
 // ReactDOM.render(<WordRelay />, document.querySelector('#root'));
-ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
+// ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
+ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
