a16ad007e56a2a5f568e0bbeeac38f9fa0c068fe
yongminiii <dev.yongmini@gmail.com>
2024-02-14 22:27:31 +0900

    feat. RockPaperScissors

--- react-ts-web-game/client.tsx
+++ react-ts-web-game/client.tsx
@@ -5,8 +5,10 @@ import GuGuDan from './1_GuGuDan/GuGuDan';
 import WordRelay from './2_WordRelay/WordRelay';
 import NumberBaseball from './3_NumberBaseball/NumberBaseball';
 import ResponseCheck from './4_ResponseCheck/ResponseCheck';
+import RockPaperScissors from './5_RockPaperScissors/RockPaperScissors';
 
 // ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
 // ReactDOM.render(<WordRelay />, document.querySelector('#root'));
 // ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
-ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
+// ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
+ReactDOM.render(<RockPaperScissors />, document.querySelector('#root'));
