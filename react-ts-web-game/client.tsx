c0a571792f277dc3dfe0c3ec2255099f598539f4
yongminiii <dev.yongmini@gmail.com>
2024-02-15 22:03:03 +0900

    feat. TicTacToe

--- react-ts-web-game/client.tsx
+++ react-ts-web-game/client.tsx
@@ -7,10 +7,12 @@ import NumberBaseball from './3_NumberBaseball/NumberBaseball';
 import ResponseCheck from './4_ResponseCheck/ResponseCheck';
 import RockPaperScissors from './5_RockPaperScissors/RockPaperScissors';
 import Lotto from './6_Lotto/Lotto';
+import TicTacToe from './7_TicTacToe/TicTacToe';
 
 // ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
 // ReactDOM.render(<WordRelay />, document.querySelector('#root'));
 // ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
 // ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
 // ReactDOM.render(<RockPaperScissors />, document.querySelector('#root'));
-ReactDOM.render(<Lotto />, document.querySelector('#root'));
+// ReactDOM.render(<Lotto />, document.querySelector('#root'));
+ReactDOM.render(<TicTacToe />, document.querySelector('#root'));
